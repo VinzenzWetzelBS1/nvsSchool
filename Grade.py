77ed21237c7321620924d40f9d7c4ce732869f63
Vinzenz Wetzel <wetzel@intercorp.de>
2023-03-23 08:30:47 +0100

    first commit

---
+++ Grade.py
@@ -0,0 +1,6 @@
+class Grade:
+    def __init__(self, weight, date, level, course):
+        self.weight = weight
+        self.date = date
+        self.level = level
+        self.course = course
---
+++ Klasse.py
@@ -0,0 +1,6 @@
+class Klasse:
+
+    students = []
+
+    def __init__(self, name):
+        self.name = name
